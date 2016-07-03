049d730172b866e45af0c4c62f862824e9abd5f0
Thomas Castiglione <castiglione@mac.com>
2016-07-03 21:15:50 +0800

    cut out the plugin stuff and the frontends - not required for repro

--- Lib.Plugin/QuoteFormatter.vb
+++
@@ -1,9 +0,0 @@
-﻿Imports [Lib].Common
-
-Public Class QuoteFormatter
-    Implements IFormatter
-
-    Public Function Format(number As Integer) As String Implements IFormatter.Format
-        Return $"'{number}'"
-    End Function
-End Class
