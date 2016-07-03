3a9660c595d48051cb2953ced0aee9d68bcfa649
Thomas Castiglione <castiglione@mac.com>
2016-07-03 17:47:57 +0800

    add a web-specific library and frontend

---
+++ Lib.Web/DebugController.cs
@@ -0,0 +1,24 @@
+﻿using Lib.Common;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Lib.Web
+{
+    [Route("api/[controller]")]
+    public class DebugController : Controller
+    {
+        private readonly IFormatter _formatter;
+
+        public DebugController(IFormatter formatter)
+        {
+            _formatter = formatter;
+        }
+
+        // GET api/value
+        [HttpGet]
+        public string Get()
+        {
+            Printer.Print(_formatter);
+            return "Check output window for result.";
+        }
+    }
+}
---
+++ WebApp/Startup.cs
@@ -0,0 +1,38 @@
+﻿using Lib.Common;
+using Lib.Common.Formats;
+using Lib.Web;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using System.IO;
+
+namespace WebApp
+{
+    public class Startup
+    {
+        public static void Main(string[] args)
+        {
+            var host = new WebHostBuilder()
+                .UseKestrel()
+                .UseContentRoot(Directory.GetCurrentDirectory())
+                .UseIISIntegration()
+                .UseStartup<Startup>()
+                .Build();
+
+            host.Run();
+        }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc().AddApplicationPart(typeof(DebugController).Assembly);
+            services.AddSingleton<IFormatter>(new QuoteFormatter());
+        }
+
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+        {
+            loggerFactory.AddDebug();
+            app.UseMvc();
+        }
+    }
+}
